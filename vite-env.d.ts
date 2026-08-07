dcfcdbf8b7c1a9a3cbce8f6694d8d3b26d280688
EinsRos/F1725 <f1725developmenttechnologies@gmail.com>
2026-08-07 10:55:35 -0500

    Actualizar vite-env.d.ts

--- vite-env.d.ts
+++ vite-env.d.ts
@@ -14,3 +14,19 @@ declare module '*.svg?import&react' {
   const src: string;
   export default src;
 }
+/// <reference types="vite/client" />
+/// <reference types="vite-plugin-svgr/client" />
+
+declare module '*.svg?react' {
+  import * as React from 'react';
+  export const ReactComponent: React.FunctionComponent<React.SVGProps<SVGSVGElement>>;
+  const src: string;
+  export default src;
+}
+
+declare module '*.svg?import&react' {
+  import * as React from 'react';
+  export const ReactComponent: React.FunctionComponent<React.SVGProps<SVGSVGElement>>;
+  const src: string;
+  export default src;
+}
